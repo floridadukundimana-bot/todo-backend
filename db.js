81c1cb33b61e910f462762ef12f6aec9ef2448f1
Florida Dukundimana <floridadukundimana@gmail.com>
2026-09-25 09:37:45 +0200

    Add Render port support

--- db.js
+++ db.js
@@ -1,20 +1,19 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "todo_db",
-    password: "Florida@2002",
-    port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.query("SELECT NOW()", (err, result) => {
-    if (err) {
-        console.error("Database connection failed:", err);
-    } else {
-        console.log("Database connected successfully!");
-        console.log(result.rows);
-    }
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Database connected successfully!");
+    console.log(result.rows);
+  }
 });
 
 module.exports = pool;
